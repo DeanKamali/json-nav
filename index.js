78637d83d140f624ef05096883f7ead6eb693bf7
Deano <dean.kamali@gmail.com>
2023-03-08 08:56:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function query(json, queryStr) {
+    const queryArr = queryStr.split('.');
+    let result = [json];
+
+    for (let i = 0; i < queryArr.length; i++) {
+        const currentQuery = queryArr[i];
+        const nextResult = [];
+
+        for (let j = 0; j < result.length; j++) {
+            const currentResult = result[j];
+
+            if (Array.isArray(currentResult)) {
+                for (let k = 0; k < currentResult.length; k++) {
+                    const currentElement = currentResult[k];
+                    if (typeof currentElement === 'object' && currentElement[currentQuery]) {
+                        nextResult.push(currentElement[currentQuery]);
+                    }
+                }
+            } else if (typeof currentResult === 'object' && currentResult[currentQuery]) {
+                nextResult.push(currentResult[currentQuery]);
+            }
+        }
+
+        result = nextResult;
+    }
+
+    return result;
+}
